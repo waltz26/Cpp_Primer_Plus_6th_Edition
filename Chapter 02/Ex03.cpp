ff215303074accdf77bf0879989f7930707f4798
waltz26 <qq1932048447@163.com>
2024-02-28 16:39:30 +0800

    第二章

--- Chapter 02/Ex03.cpp
+++ Chapter 02/Ex03.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
 
 void func1(void);
--- Chapter 02/Ex07.cpp
+++ Chapter 02/Ex07.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
 
 void show_time(int hour,int minute);
